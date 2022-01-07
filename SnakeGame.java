c0103b87e9db320647e5f893f3fa8bdc09c95bc5
MBrayanA <97263264+MBrayanA@users.noreply.github.com>
2022-01-07 02:50:49 -0800

    Add files via upload

---
+++ SnakeGame.java
@@ -0,0 +1,9 @@
+
+public class SnakeGame{
+    public static void main(String[] args){
+
+        //GameFrame frame = new GameFrame(); //creating an instance of game frame
+        new GameFrame();
+
+    }
+}
